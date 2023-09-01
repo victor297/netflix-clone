8bac5cbffb414608600c065038b203ee02950f6c
victor297 <davidvictor297.com>
2023-09-01 17:01:48 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import SignIn from "./Components/Auth/SignIn/SignIn";
+import SignUp from "./Components/Auth/SignUp/SignUp";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import { useStateValue } from "./ContextApi/stateProvider";
+import Home from "./Components/Home/Home";
+
+import { auth } from "./Config/firebase";
+import { actionType } from "./ContextApi/reducer";
+import SearchResult from "./Components/SearchResult/SearchResult";
+import Loading from "./Components/Loading/Loading";
+import PageNotFound404 from "./Components/PageNotFound404/PageNotFound404";
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+  const [loading, setLoading] = useState(true);
+  console.log(
+    process.env.REACT_APP_FIREBASE_API_KEY,
+    process.env.REACT_APP_FIREBASE_PROJECTID
+  );
+  useEffect(() => {
+    setLoading(true);
+    //to retain the user signed after refresh without localstorage
+
+    const listener = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: actionType.SET_USER,
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: actionType.SET_USER,
+          user: null,
+        });
+      }
+      setLoading(false);
+    });
+
+    //clean up
+    return () => listener();
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <Loading
+        addStyle={{
+          position: "fixed",
+          top: "0",
+          right: "0",
+          bottom: "0",
+          left: "0",
+          zIndex: "99999",
+        }}
+      />
+    );
+  } else {
+    return (
+      <div className='app'>
+        <Router basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path='/' exact>
+              {!user ? <LandingPage /> : <Home />}
+            </Route>
+            <Route path='/browse/:param' exact>
+              {!user ? <Redirect to='/signin' /> : <Home />}
+            </Route>
+            <Route path='/signin'>
+              {!user ? <SignIn /> : <Redirect to='/' />}
+            </Route>
+            <Route path='/signup'>
+              {!user ? <SignUp /> : <Redirect to='/' />}
+            </Route>
+            <Route path='/search/:query' exact>
+              {!user ? <Redirect to='/signin' /> : <SearchResult />}
+            </Route>
+            <Route path='*'>
+              <PageNotFound404 />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Config/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
